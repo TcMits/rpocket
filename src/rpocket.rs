8df6b3986ceba909bd6d14b7b7603a32ec7784c9
Lâm Hoàng Phúc <64113128+TcMits@users.noreply.github.com>
2023-03-17 13:39:07 +0700

    feat: seccond commit

--- src/rpocket.rs
+++ src/rpocket.rs
@@ -1,30 +1,61 @@
 use crate::{store, store::auth_storage};
 
 pub struct PocketBase<'a> {
+    base_url: &'static str,
+    lang: &'static str,
     auth_state: auth_storage::AuthStorage<'a>,
 }
 
 impl<'a> PocketBase<'a> {
-    /// Create a new PocketBase.
+    /// Create a new PocketBase client.
+    /// base_url: the base URL for the PocketBase server.
+    /// lang: the language to use.
     /// storage: the storage to use.
-    pub fn new(storage: &'a dyn store::Storage) -> Self {
-        return crate::rpocket::PocketBase::new_with_auth_state(auth_storage::AuthStorage::new(storage));
+    pub fn new(
+        base_url: &'static str,
+        lang: &'static str,
+        storage: &'a dyn store::Storage,
+    ) -> Self {
+        return crate::rpocket::PocketBase::new_with_auth_state(
+            base_url,
+            lang,
+            auth_storage::AuthStorage::new(storage),
+        );
     }
 
     /// Create a new PocketBase with an existing AuthStorage.
-    pub fn new_with_auth_state(auth_state: auth_storage::AuthStorage<'a>) -> Self {
-        return PocketBase { auth_state };
+    /// base_url: the base URL for the PocketBase server.
+    /// lang: the language to use.
+    /// storage: the storage to use.
+    pub fn new_with_auth_state(
+        base_url: &'static str,
+        lang: &'static str,
+        auth_state: auth_storage::AuthStorage<'a>,
+    ) -> Self {
+        return PocketBase {
+            base_url,
+            lang,
+            auth_state,
+        };
+    }
+
+    /// Get the AuthStorage.
+    pub fn auth_state(&self) -> &auth_storage::AuthStorage {
+        return &self.auth_state;
+    }
+
+    /// Get the base URL.
+    pub fn base_url(&self) -> &'static str {
+        return self.base_url;
+    }
+
+    /// Get the language.
+    pub fn lang(&self) -> &'static str {
+        return self.lang;
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
-
-    #[test]
-    fn test_pocket_base() {
-        let store = crate::store::MemoryStorage::new();
-        let pocket_base = PocketBase::new(&store);
-        assert!(pocket_base.auth_state.token().unwrap().is_none());
-    }
+    // use super::*;
 }
